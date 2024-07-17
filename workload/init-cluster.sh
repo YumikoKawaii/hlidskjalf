745280769230e57f597539f9e068d531afd51110
Yumiko Sturluson <vu.la@teko.vn>
2024-07-17 23:56:54 +0700

    update sh

--- workload/init-cluster.sh
+++ workload/init-cluster.sh
@@ -6,3 +6,9 @@ k3d cluster create hlidskjalf
 k3d node create worker-1 --cluster=hlidskjalf --role=agent
 k3d node create worker-2 --cluster=hlidskjalf --role=agent
 k3d node create worker-3 --cluster=hlidskjalf --role=agent
+# taint master
+kubectl taint nodes k3d-hlidskjalf-server-0 node-role.kubernetes.io/master=:NoSchedule
+# label workers
+kubectl label node k3d-worker-1-0 node-role.kubernetes.io/worker=worker
+kubectl label node k3d-worker-2-0 node-role.kubernetes.io/worker=worker
+kubectl label node k3d-worker-3-0 node-role.kubernetes.io/worker=worker
