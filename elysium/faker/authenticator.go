5d9587c4f3727af6e6b8a9c88d5166b3cfda490d
Yumiko Sturluson <yumiko.stl@gmail.com>
2024-10-27 14:44:06 +0700

    fake authenticator data

--- elysium/faker/authenticator.go
+++ elysium/faker/authenticator.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"context"
+	"elysium.com/applications/utils"
 	pb "elysium.com/pb/authenticator"
 	"encoding/csv"
 	"fmt"
-	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"os"
 )
 
 type RegisterInformation struct {
+	Id       string `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type Account struct {
+	Id             string `json:"id,omitempty"`
+	HashedEmail    string `json:"hashed_email,omitempty"`
+	HashedPassword string `json:"hashed_password,omitempty"`
+}
+
+func (a *Account) ToSQLValue() string {
+	return fmt.Sprintf("(\"%s\",\"%s\",\"%s\")", a.Id, a.HashedEmail, a.HashedPassword)
+}
+
+func (a *Account) ToCSV() string {
+	return fmt.Sprintf("%s,%s,%s", a.Id, a.HashedEmail, a.HashedPassword)
 }
 
 func initAuthenticator() pb.AuthenticatorClient {
@@ -45,21 +59,78 @@ func main() {
 			continue
 		}
 		registerInfos = append(registerInfos, RegisterInformation{
-			Email:    record[0],
-			Password: record[1],
+			Id:       record[0],
+			Email:    record[1],
+			Password: record[2],
 		})
 	}
 
-	ctx := context.Background()
-	authenticator := initAuthenticator()
+	hashKey := "123"
+
+	accounts := make([]Account, 0)
 	for _, registerInfo := range registerInfos {
-		_, err := authenticator.Signup(ctx, &pb.SignupRequest{
-			Email:    registerInfo.Email,
-			Password: registerInfo.Password,
+		accounts = append(accounts, Account{
+			Id:             registerInfo.Id,
+			HashedEmail:    utils.HashString(registerInfo.Email, hashKey),
+			HashedPassword: utils.HashString(registerInfo.Password, hashKey),
 		})
-		if err != nil {
-			logrus.Error(err.Error())
+	}
+
+	csvFile, err := os.Create(fmt.Sprintf("%s%s", cwd, "/faker/migrations/accounts.csv"))
+	if err != nil {
+		panic(err)
+	}
+
+	writer := csv.NewWriter(csvFile)
+	defer writer.Flush()
+
+	header := []string{"id", "hashed_email", "hashed_password"}
+	if err := writer.Write(header); err != nil {
+		fmt.Println("Error writing header:", err)
+		return
+	}
+
+	for _, record := range accounts {
+		row := []string{record.Id, record.HashedEmail, record.HashedPassword}
+		if err := writer.Write(row); err != nil {
+			fmt.Println("Error writing record:", err)
+			return
 		}
 	}
+	//stmts := make([]string, 0)
+	//// divide by batches
+	//batchSize := 1000
+	//for {
+	//	batch := accounts[:batchSize]
+	//	accounts = accounts[batchSize:]
+	//
+	//	values := make([]string, 0)
+	//	for _, account := range batch {
+	//		values = append(values, account.ToSQLValue())
+	//	}
+	//
+	//	stmt := fmt.Sprintf(""+
+	//		"insert into `accounts`(id, hashed_email, encrypted_email, hashed_password)"+
+	//		"values %s;",
+	//		strings.Join(values, ","),
+	//	)
+	//
+	//	stmts = append(stmts, stmt)
+	//	if len(accounts) == 0 {
+	//		break
+	//	}
+	//}
+	//
+	//migrationFile, err := os.Create(fmt.Sprintf("%s%s", cwd, "/faker/migrations/accounts.txt"))
+	//if err != nil {
+	//	panic(err)
+	//}
+	//for _, stmt := range stmts {
+	//	_, err := migrationFile.WriteString(stmt + "\n")
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
 
+	fmt.Println("Done")
 }
