0b588806b0a53e51546c39b39f48a0dbe4542902
Misteln <vu.la@teko.vn>
2024-10-29 10:22:30 +0700

    fictio

--- elysium/applications/mockers/fictio/cmd/main.go
+++ elysium/applications/mockers/fictio/cmd/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"elysium.com/applications/mockers/fictio/config"
+	"elysium.com/applications/mockers/fictio/server"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+
+	cfg, kongCtx := config.Initialize()
+	switch cmd := kongCtx.Command(); cmd {
+	case "serve":
+		server.Serve(cfg)
+	default:
+		logrus.Fatalf("unknown option: %s", cmd)
+	}
+}
