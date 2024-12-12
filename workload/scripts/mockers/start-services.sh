0dd8578c7b2b130bd5fbe17c3c04f3a1e850a7f3
Misteln <vu.la@teko.vn>
2024-12-13 05:06:49 +0700

    update workload

--- workload/scripts/mockers/start-services.sh
+++
@@ -1,6 +0,0 @@
-kubectl apply -f ../applications/mockers/echo/service.yaml
-kubectl apply -f ../applications/mockers/faux/service.yaml
-kubectl apply -f ../applications/mockers/fictio/service.yaml
-kubectl apply -f ../applications/mockers/mimic/service.yaml
-kubectl apply -f ../applications/mockers/virtual/service.yaml
-kubectl apply -f ../applications/mockers/presentation/service.yaml
---
+++ workload/scripts/start-deployments.sh
@@ -0,0 +1,6 @@
+kubectl apply -f ./applications/mockers/echo/deployment.yaml
+kubectl apply -f ./applications/mockers/faux/deployment.yaml
+kubectl apply -f ./applications/mockers/fictio/deployment.yaml
+kubectl apply -f ./applications/mockers/mimic/deployment.yaml
+kubectl apply -f ./applications/mockers/virtual/deployment.yaml
+kubectl apply -f ./applications/mockers/presentation/deployment.yaml
