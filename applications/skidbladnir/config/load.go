44a16dcf1239439b694ab4e2d213d901cb8e787a
YumikoKawaii <yumiko.stl@gmail.com>
2026-02-13 20:38:45 +0700

    @.@

--- applications/skidbladnir/config/load.go
+++ applications/skidbladnir/config/load.go
@@ -17,7 +17,6 @@ func Load() (*Application, error) {
 
 	viper.AddConfigPath("./")
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "__"))
-	viper.SetEnvPrefix("SKIDBLADNIR")
 	viper.AutomaticEnv()
 	c := loadDefault()
 	if configBuffer, err := json.Marshal(c); err != nil {
